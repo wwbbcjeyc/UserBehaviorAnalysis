1a8bdbe48ae26d456c5df9bbb0bc540a43ce2201
wwb <wwb_bcjeyc@163.com>
2020-07-17 01:03:47 +0800

    "marketanalysis"

--- MarketAnalysis/src/main/scala/com/zjtd/marketanalysis/bean/MarketingViewCount.scala
+++ MarketAnalysis/src/main/scala/com/zjtd/marketanalysis/bean/MarketingViewCount.scala
@@ -3,4 +3,4 @@ package com.zjtd.marketanalysis.bean
 /**
   * 分渠道统计样例类输出结果
   */
-case class MarketingCountView(windowStart: Long, windowEnd: Long, channel: String, behavior: String, count: Long)
+case class MarketingViewCount(windowStart: Long, windowEnd: Long, channel: String, behavior: String, count: Long)
